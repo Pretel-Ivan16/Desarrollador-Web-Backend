f66788b67a20d6097fd807e6d5ef1c18411d4eba
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-01-27 13:02:22 -0300

    Clase 4 - Complemento

--- Clase 4 Complemento/src/main.ts
+++ Clase 4 Complemento/src/main.ts
@@ -1,25 +1,121 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counterMore.ts'
+/* 
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <div></div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+SISTEMA DE PLATAFORMA DE STREAMING
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+Contenido: respresenta cualquier contenido disponible en la plataforma.
+Propiedades:
+  - id
+  - titulo
+  - duración (en minutos)
+  - genero
+  - calificación
+
+Usuario: representa a un usuario de la plataforma
+Propiedades:
+  - id
+  - nombre
+  - plan (free / premium)
+  -lista[]: Contenido
+
+Reproductor: se encarga de administrar el contenido del usuario.
+Propiedades:
+  - limite_contenido
+  - contenidos[]: Contenido
+Parámetro configurable:
+  - limite de contenidos guardados
+
+Metodos:
+  agregarContenido(contenido)
+  - Si el contenido ya está en la lista (por id), no lo agrega
+  - Si no está:
+    - verifica si no se alcanzó el límite
+    - si hay espacio, lo agrega
+
+  elminarContenido(contenido_id)
+  - Elimina un contenido de la lista
+
+  reproducirContenido(contenido_id)
+  - Busca el contenido
+  - Si existe, muestra que se está reproduciendo
+  - Si no existe, muestra un mensaje de error
+
+*/
+
+type calificacion = "G" | "PG" | "R";
+type plan = "free" | "premium";
+
+class Contenido{
+  id: number
+  titulo: string
+  duracion: number
+  genero: string
+  calificacion: calificacion
+
+  constructor(
+    id: number,
+    titulo: string,
+    duracion: number,
+    genero: string,
+    calificacion: calificacion
+  ){
+    this.id = id
+    this.titulo = titulo
+    this.duracion = duracion
+    this.genero = genero
+    this.calificacion = calificacion
+  }
+}
+
+class Usuario{
+  id: number | string
+  nombre: string
+  plan: plan
+  lista: Contenido[]
+
+  constructor(
+    id: number | string,
+    nombre: string,
+    plan: plan,
+    lista: Contenido[]
+  ){
+    this.id = id
+    this.nombre = nombre
+    this.plan = plan
+    this.lista = lista
+  }
+}
+
+class Reproductor{
+  private limite_contenido: number
+  private contenido: Contenido[] = []
+
+  constructor(limite: number){
+    this.limite_contenido = limite
+    this.contenido = []
+  }
+
+  agregarContenido(contenido: Contenido): void{
+    const contenidoExistente = this.contenido.find(i => i.id === contenido.id)
+    if (contenidoExistente){
+      return
+    }
+    if (!contenidoExistente){
+      if(this.contenido.length < this.limite_contenido){
+        this.contenido.push(contenido)
+      } else {
+        console.log("Error")
+      }
+      return
+    }
+  }
+
+  eliminarContenido(contenido_id: number): void{
+    const contenidoExiste = this.contenido.findIndex(c => c.id === contenido_id)
+    if(contenidoExiste !== 1){
+      this.contenido.splice(contenidoExiste, 1)
+      console.log("Contenido Eliminado")
+    } else{
+      console.log("Error: Contenido no encontrado");
+    }
+  }
+}
