52548464293a7af17520b0b8297987e437f09941
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-04-11 19:17:54 -0300

    feat: implement Express app initialization and server listening

--- Clase 13 - Introduccion a NodeJs/src/server.js
+++ Clase 13 - Introduccion a NodeJs/src/server.js
@@ -1,5 +1,18 @@
 import ENVIRONMENT from "./config/environment.config.js";
+import express from 'express'
 
-console.log("Hola Mundo");
+// Crear una app en express
 
-console.log(ENVIRONMENT);
+const app = express()
+
+//Hace que nuestra app en express se escuche en cierto puerto de nuestra PC
+//Espera recibir 2 parámetros
+//  1: puerto => en que direciion se ejcuta la app
+//  2: Callback FN => una vez ña app se ejecute en ese puerto se activara la callback
+
+app.listen(
+  ENVIRONMENT.PORT,
+  () => (
+    console.log(`La aplicación se está ejecutando correctamente en el puerto ${ENVIRONMENT.PORT}`)
+  )
+)
