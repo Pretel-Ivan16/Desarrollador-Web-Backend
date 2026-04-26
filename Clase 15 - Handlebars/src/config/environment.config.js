7ae043241f923d8fb2426eecd78f432a16520c45
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-04-26 17:17:50 -0300

    feat: add PORT to ENVIRONMENT configuration in environment.config.js

--- Clase 15 - Handlebars/src/config/environment.config.js
+++ Clase 15 - Handlebars/src/config/environment.config.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const ENVIRONMENT = {
-    MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING
+    MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
+    PORT: process.env.PORT
 }
 
 export default ENVIRONMENT
