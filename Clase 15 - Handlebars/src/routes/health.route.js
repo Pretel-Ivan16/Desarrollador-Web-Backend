0ab2223761f64b4bf53817c8232794a13d036ad6
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-04-28 12:53:59 -0300

    fix: remove unused import and format health check page response

--- Clase 15 - Handlebars/src/routes/health.route.js
+++ Clase 15 - Handlebars/src/routes/health.route.js
@@ -1,4 +1,4 @@
-import express, { response } from 'express'
+import express from 'express'
 import userRepository from '../repository/user.repository.js'
 import healthController from '../../controllers/health.controller.js'
 
@@ -7,6 +7,25 @@ const healthRouter = express.Router()
 healthRouter.get(
   '/',
   healthController.getApi
+)
+
+healthRouter.get(
+  '/page',
+  (request, response) => {
+    return response.status(200).send(
+      `
+        <html>
+          <head>
+            <title>Health Check</title>
+          </head>
+          <body>
+            <h1>Health Check</h1>
+            <p>API is running correctly.</p>
+          </body>
+        </html>
+      `
+    )
+  }
 )
 
 healthRouter.get(
