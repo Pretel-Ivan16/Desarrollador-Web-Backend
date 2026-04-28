cd100cb8baaba08832bf06343a6caf1304069aa3
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-04-28 12:54:07 -0300

    feat: configure Handlebars as the view engine and set up views directory

--- Clase 15 - Handlebars/src/main.js
+++ Clase 15 - Handlebars/src/main.js
@@ -6,20 +6,55 @@ import WorkspaceMember from "./models/workspaceMember.model.js"
 import workspaceMemberRepository from "./repository/member.repository.js"
 import userRepository from "./repository/user.repository.js"
 import workspaceRepository from "./repository/workspace.repository.js"
-import express, { response } from 'express';
+import express from 'express';
 import healthRouter from "./routes/health.route.js"
+import { engine as handlebarsEngine } from 'express-handlebars'
 
 connectMongoDB()
 
 const app = express()
 
-// Delegamos las consultas que vengan sobre '/api/health' al healthRouter
+//Configuramos handlebars como motor de vistas
+app.engine('handlebars', handlebarsEngine());
 
+// Indicamos que el motor de vistas es handlebars (archivos con extension .handlebars)
+app.set('view engine', 'handlebars');
+
+// Indicamos que las vistas van a estar en la carpeta 'views'
+app.set('views', './views');
+
+/* 
+Delegamos las consultas que vengan sobre '/api/health' al healthRouter
+*/
 app.use('/api/health', healthRouter)
 
-app.listen(ENVIRONMENT.PORT,
-  () => {
-    console.log('La app se está escuchando en el puerto ' + ENVIRONMENT.PORT);
-    
-  }
+app.get('/', (request, response) => {
+    return response.render(
+        'home', 
+        {
+            username: 'Pepito',
+            isVip: false,
+            favorite_foods: [
+                {
+                    nombre: 'Milanesa con papas fritas',
+                    calorias: 1200
+                }, 
+                {
+                    nombre: 'Pizza',
+                    calorias: 1000
+                }, 
+                {
+                    nombre: 'Hamburguesa',
+                    calorias: 600
+                }
+            ]
+        }
+    )
+})
+
+app.listen(
+    ENVIRONMENT.PORT, 
+    () => {
+        console.log('La aplicacion se esta escuchando en el puerto ' + ENVIRONMENT.PORT)
+    }
 )
