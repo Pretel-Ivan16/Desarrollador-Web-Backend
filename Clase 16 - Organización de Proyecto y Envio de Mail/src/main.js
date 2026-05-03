ee8c37e75791f949a232400deb74b4b3954e16d2
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-03 14:55:24 -0300

    refactor: remove Handlebars view engine setup and related code

--- Clase 16 - Organización de Proyecto y Envio de Mail/src/main.js
+++ Clase 16 - Organización de Proyecto y Envio de Mail/src/main.js
@@ -8,49 +8,21 @@ import userRepository from "./repository/user.repository.js"
 import workspaceRepository from "./repository/workspace.repository.js"
 import express from 'express';
 import healthRouter from "./routes/health.route.js"
-import { engine as handlebarsEngine } from 'express-handlebars'
+import authRouter from "./routes/auth.route.js"
 
 connectMongoDB()
 
 const app = express()
 
-//Configuramos handlebars como motor de vistas
-app.engine('handlebars', handlebarsEngine());
-
-// Indicamos que el motor de vistas es handlebars (archivos con extension .handlebars)
-app.set('view engine', 'handlebars');
-
-// Indicamos que las vistas van a estar en la carpeta 'views'
-app.set('views', './views');
+// Middleware para parsear JSON
+app.use(express.json())
 
 /* 
 Delegamos las consultas que vengan sobre '/api/health' al healthRouter
 */
-app.use('/api/health', healthRouter)
 
-app.get('/', (request, response) => {
-    return response.render(
-        'home', 
-        {
-            username: 'Pepito',
-            isVip: false,
-            favorite_foods: [
-                {
-                    nombre: 'Milanesa con papas fritas',
-                    calorias: 1200
-                }, 
-                {
-                    nombre: 'Pizza',
-                    calorias: 1000
-                }, 
-                {
-                    nombre: 'Hamburguesa',
-                    calorias: 600
-                }
-            ]
-        }
-    )
-})
+app.use('/api/health', healthRouter)
+app.use('/api/auth', authRouter)
 
 app.listen(
     ENVIRONMENT.PORT, 
