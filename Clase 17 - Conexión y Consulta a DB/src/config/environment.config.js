394209b5855446c2686d4c2e5d7d07c15dbeb252
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-04 22:26:46 -0300

    feat: add JWT_SECRET_KEY to environment configuration

--- Clase 17 - Conexión y Consulta a DB/src/config/environment.config.js
+++ Clase 17 - Conexión y Consulta a DB/src/config/environment.config.js
@@ -7,7 +7,8 @@ const ENVIRONMENT = {
     PORT: process.env.PORT,
     MAIL_USER: process.env.MAIL_USER,
     MAIL_PASSWORD: process.env.MAIL_PASSWORD,
-    URL_BACKEND: process.env.URL_BACKEND
+    URL_BACKEND: process.env.URL_BACKEND,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
 }
 
 export default ENVIRONMENT
